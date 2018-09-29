567a8ed9ed6052a68ffe16dbce92c44106852708
hrg921 <hrg921@gmail.com>
2018-09-29 16:48:00 +0900

    add travis-ci configuration
    update gitignore to ignore build results

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp index.html build
+parcel build AboutPage/index.html --out-dir build/AboutPage
+parcel build LandingPage/index.html --out-dir build/LandingPage
+parcel build MyPage/index.html --out-dir build/MyPage
+parcel build SessionPage/index.html --out-dir build/SessionPage
