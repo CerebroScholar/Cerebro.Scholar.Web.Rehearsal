e674bcff13bd58a2c1fc3d225cfc61743deecbe1
hrg921 <hrg921@gmail.com>
2018-09-29 18:07:26 +0900

    fix build script to add public url for each page

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir build
 cp index.html build
-parcel build AboutPage/index.html --out-dir build/AboutPage
-parcel build LandingPage/index.html --out-dir build/LandingPage
-parcel build MyPage/index.html --out-dir build/MyPage
-parcel build SessionPage/index.html --out-dir build/SessionPage
+parcel build AboutPage/index.html --out-dir build/AboutPage --public-url ./
+parcel build LandingPage/index.html --out-dir build/LandingPage --public-url ./
+parcel build MyPage/index.html --out-dir build/MyPage --public-url ./
+parcel build SessionPage/index.html --out-dir build/SessionPage --public-url ./
