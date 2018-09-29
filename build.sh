03232685ede8667ffa17047a24a7968ccb066868
hrg921 <hrg921@gmail.com>
2018-09-29 18:01:38 +0900

    Revert "fix build script to add public url for each page"
    This reverts commit 2ccba9f594ccf96e02b95d0033d3298ef9b3939e.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mkdir build
 cp index.html build
-parcel build AboutPage/index.html --out-dir build/AboutPage --public-url ./AboutPage/
-parcel build LandingPage/index.html --out-dir build/LandingPage --public-url ./LandingPage/
-parcel build MyPage/index.html --out-dir build/MyPage --public-url ./MyPage/
-parcel build SessionPage/index.html --out-dir build/SessionPage --public-url ./SessionPage/
+parcel build AboutPage/index.html --out-dir build/AboutPage
+parcel build LandingPage/index.html --out-dir build/LandingPage
+parcel build MyPage/index.html --out-dir build/MyPage
+parcel build SessionPage/index.html --out-dir build/SessionPage
